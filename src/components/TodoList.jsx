83effbbfd34eca3ee611990674944c43b132104e
Saurabh <devsaurabh086@gmail.com>
2025-03-01 22:48:56 +0530

    List marked done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,43 +1,26 @@
-//Todo card has to be rendered inside TodoList , i..e inside this particular component .
-
+//The TodoList is going to be dependent on the tab that's currently opened :
 import TodoCard from "./TodoCard";
-import PropTypes from "prop-types"; 
 
-//This functional component has to communicate with the Tabs Component 
-//Because the number shown in the Tabs will decide the list that has to be displayed!
+ 
 export default function TodoList(props) {
 
-    const { todos } = props;
+    const { todos , selectedTab} = props;
+    const filterTodosList = selectedTab == "All" ? 
+    todos :
+    selectedTab == "Completed" ? 
+    todos.filter(val=>val.complete) : 
+    todos.filter(val=>!val.complete);
 
-    const tab = "All"
-    let filterTodoList = 0;
-    if (tab === "All") {
-        filterTodoList = todos;
-    }
-    else if (tab === "completed") {
-        filterTodoList = todos.filter(val=>val.complete)
-    }
-    else {
-        filterTodoList = todos.filter(val => !val.complete);
-    }
     
-
     return (
         <>
             {
-                filterTodoList.map((todo , todoIndex) => {
+                filterTodosList.map((todo , todoIndex) => {
                     return (
-                        <TodoCard
-                            key={todoIndex}
-                            todo={todo}
-                        />
+                        <TodoCard key={ todoIndex} todo={todo} {...props} todoIndex={todos.findIndex(val=>val.input==todo.input)}/>
                     )
                 })
             }
         </>
-    )
-}
-
-TodoList.propTypes = {
-    todos: PropTypes.array.isRequired,
-}
+     )
+ }
